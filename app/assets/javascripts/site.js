60d1cadb0ad606ab5408533909cd0d9bd777a4eb
moritzdaniels <moritzdaniels@gmail.com>
2016-10-29 20:47:56 +0200

    6.10 version 1

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,5 +1,20 @@
-$(document).on('turbolinks:load ajaxSuccess', function(){
-    $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
-    $('.rated').raty({ path: '/assets', readOnly: true, score: function() {return $(this).attr('data-score');}});
-    $('.img-zoom').elevateZoom();
+var refreshRating = function() {
+  $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+  $('.rated').raty({ path: '/assets',
+    readOnly: true,
+    score: function() {
+      return $(this).attr('data-score');
+    }
+  });
+};
+
+$(document).on('turbolinks:load', function(){
+  $('.img-zoom').elevateZoom();
+  $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+  $('.rated').raty({ path: '/assets',
+  readOnly: true,
+  score: function() {
+    return $(this).attr('data-score');
+  }
+});
 });
