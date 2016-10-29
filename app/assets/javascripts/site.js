79d993f8ffa563654de597ab2bbc806bd18ea85e
moritzdaniels <moritzdaniels@gmail.com>
2016-10-29 17:16:37 +0200

    6.9 version 1

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,4 +1,5 @@
-$(document).on('turbolinks:load', function(){
+$(document).on('turbolinks:load ajaxSuccess', function(){
     $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
     $('.rated').raty({ path: '/assets', readOnly: true, score: function() {return $(this).attr('data-score');}});
+    $('.img-zoom').elevateZoom();
 });
