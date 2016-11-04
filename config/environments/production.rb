05c6d6834842a3e26b959de3240991ae7e7357aa
moritzdaniels <moritzdaniels@gmail.com>
2016-11-04 21:50:30 +0100

    6.11 version 1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,15 @@ Rails.application.configure do
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
 
+  config.cache_store = :dalli_store,
+                    (ENV["MEMCACHIER_SERVERS"] || "").split(","),
+                    {:username => ENV["MEMCACHIER_USERNAME"],
+                     :password => ENV["MEMCACHIER_PASSWORD"],
+                     :failover => true,
+                     :socket_timeout => 1.5,
+                     :socket_failure_delay => 0.2,
+                     :down_retry_delay => 60
+                    }
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
