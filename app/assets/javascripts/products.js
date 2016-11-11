776f852b89c4dc924444a73948a20b4206ca5aec
moritzdaniels <moritzdaniels@gmail.com>
2016-11-11 14:13:11 +0100

    6.13 version 1

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,3 +1,7 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+received: function() {
+  // Called when there's incoming data on the websocket for this channel
+  $(".alert.alert-info").show();
+}
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,4 +93,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.web_socket_server_url = "wss://peaceful-ravine-90194.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://peaceful-ravine-90194.herokuapp.com', 'http://peaceful-ravine-90194.herokuapp.com']
 end
